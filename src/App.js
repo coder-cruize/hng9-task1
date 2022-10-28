7e710552b23e5aabfa93b992d3d71dc330bc70d6
lekan <alowoojalekan@gmail.com>
2022-10-28 16:04:07 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import "./App.css";
+import icon_i4g from "./resources/i4g.png";
+import icon_zuri from "./resources/zuri.png";
+import icon_twitter from "./resources/twitter.png";
+import icon_slack from "./resources/slack.png";
+import icon_github from "./resources/github.png";
+
+function CreateLinks({ links }) {
+  return (
+    <div className="links">
+      {links.map((val) => {
+        return (
+          <a href={val.url} className="link" key={val.id} id={val.id} title={val.subtext}>
+            {val.title}
+          </a>
+        );
+      })}
+    </div>
+  );
+}
+function App() {
+  const links = [
+    { title: "Twitter Link", url: "https://twitter.com/LekanA_O", id: "twitter" },
+    { title: "Zuri Team", url: "https://training.zuri.team/", id: "btn__zuri" },
+    {
+      title: "Zuri Books",
+      url: "https://training.zuri.team/",
+      id: "books",
+      subtext: "This is where you find books about design and coding",
+    },
+    { title: "Python Books", url: "https://books.zuri.team/", id: "book__python" },
+    {
+      title: "Background Check for Coders",
+      url: "https://books.zuri.team/python-for-beginners?ref_id=Lekan Alowooja",
+      id: "pitch",
+      subtext: "GoodHire is an engaging and easy-to-use interface to perform background checks on coders.",
+    },
+    {
+      title: "Design Books",
+      url: "https://books.zuri.team/design-rules",
+      id: "book__design",
+      subtext:
+        "The HNG Design Rules Book gives you the tips & guidelines you need to create truly professional designs, by addressing errors that could you get you disqualified.",
+    },
+  ];
+  return (
+    <div className="App">
+      <header>
+        <img src={icon_twitter} alt="" id="profile__img" />
+        <span id="twitter">LekanA_O</span>
+        <span id="slack" style={{ display: "none" }}>
+          cruize_
+        </span>
+      </header>
+      <CreateLinks links={links} />
+      <div className="social">
+        <a href="https://hngi9.slack.com/">
+          <img src={icon_slack} alt="slack" />
+        </a>
+        <a href="https://github.com/coder-cruize">
+          <img src={icon_github} alt="github" />
+        </a>
+      </div>
+      <footer>
+        <img src={icon_zuri} alt="" />
+        <span>HNG Internship 9 Frontend Task</span>
+        <img src={icon_i4g} alt="" />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
