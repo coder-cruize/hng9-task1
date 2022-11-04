68e1926b6840b627b22af168509e1c180e8abc5d
Lekan Alowooja <codercruize@gmail.com>
2022-11-04 16:55:24 -0500

    created content and styles for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,38 @@
-function Contact(){
-  return(
-    <b>Hello from contact</b>
-  )
+function Contact() {
+  const name = "Lekan"
+  return (
+    <div className="Contact">
+      <div className="center">
+        <header>
+          <h2>Contact Me</h2>
+          <span>Hi there, contact me to ask me about anything you have in mind.</span>
+        </header>
+        <form>
+          <div class="field _small">
+            <label>First Name</label>
+            <input id="first_name" type="text" placeholder="Enter your first Name" />
+          </div>
+          <div class="field _small">
+            <label>Last Name</label>
+            <input id="last_name" type="text" placeholder="Enter your last Name" />
+          </div>
+          <div class="field">
+            <label>Email</label>
+            <input id="email" type="email" placeholder="yourname@email.com" />
+          </div>
+          <div class="field">
+            <label>Message</label>
+            <textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..." />
+          </div>
+          <div className="check">
+            <input type="checkbox" />
+            <span>You agree to providing your data to {name} who may contact you.</span>
+          </div>
+          <input id="btn__submit" type="button" value="Send message"/>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
